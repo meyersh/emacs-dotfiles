36317be3d20d4f3a8377f8bba7de41f179c6b221
Shaun Meyer <meyersh@morningside.edu>
2019-05-03 16:31:33 -0500

    Embrace doom-themes.

--- init.el
+++ init.el
@@ -59,7 +59,22 @@
 ;; Install missing packages by default.
 (setq use-package-always-ensure t)
 
-(use-package dracula-theme)
+;(use-package dracula-theme)
+(use-package doom-themes
+    :init (progn
+            ;; Global settings (defaults)
+            (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+                  doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+            ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each
+            ;; theme may have their own settings.
+            (load-theme 'doom-city-lights t))
+    :config (progn
+              ;; Enable flashing mode-line on errors
+              (doom-themes-visual-bell-config)
+
+              ;; Enable custom neotree theme
+              (doom-themes-neotree-config)))  ; all-the-icons fonts must be installed!
 
 (use-package which-key
     :config
