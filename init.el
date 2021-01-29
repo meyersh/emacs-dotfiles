0be9c00cc63785deb019905d02928995c9de1256
Shaun Meyer <meyersh@morningside.edu>
2021-01-28 20:37:26 -0600

    Change C-x b to use helm-buffers-list.

--- init.el
+++ init.el
@@ -355,6 +355,7 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x C-b") 'helm-mini)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
 
 (helm-mode 1)
 
