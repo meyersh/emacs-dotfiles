5ebc82bcaa259979b9232f1d7a8edd5bc857c174
Shaun Meyer <meyersh@morningside.edu>
2022-01-20 14:16:45 -0600

    Initial commit: custom-set-faces

--- init.el
+++ init.el
@@ -419,3 +419,9 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:foreground "#5EC4FF" :weight bold :height 2.0)))))
